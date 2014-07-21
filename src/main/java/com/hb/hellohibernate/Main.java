950b42536945ff51d16ab73c2d753dce0bb08385
disisdenciris <huseyin.bacanak@tpe.gov.tr>
2014-07-21 16:59:12 +0300

    added Main class

---
+++ src/main/java/com/hb/hellohibernate/Main.java
@@ -0,0 +1,10 @@
+package com.hb.hellohibernate;
+
+/**
+ * Created by huseyin.bacanak on 21.07.2014.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("stage 2 initialized");
+    }
+}
